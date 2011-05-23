f4cb16a5372003285743c7f6b98f6aeebb1fa2da
Nick Snyder <nickdsnyder@gmail.com>
2011-05-22 21:05:29 -0700

    add example

---
+++ example.go
@@ -0,0 +1,31 @@
+// 6g example.go; 6l example.6; time ./6.out; rm example.*.log;
+package main
+
+import (
+	"fmt"
+	"github.com/nicksnyder/rotatingfile.go"
+	"time"
+)
+
+const (
+	secondsPerFile = 2
+	format = "example.%v.log"
+	nanosecondsPerSecond = 1000000000
+)
+
+func main() {
+	beginTime := time.Seconds()
+	w := rotatingfile.NewWriter(secondsPerFile, format)
+	w.WriteString("hello\n")
+	time.Sleep(secondsPerFile*nanosecondsPerSecond)
+	w.WriteString("world\n")
+	endTime := time.Seconds()
+
+	r := rotatingfile.NewReader(secondsPerFile, format, beginTime, endTime)
+	buf := make([]byte, 12)
+	_, err := r.Read(buf)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%s", buf)
+}
